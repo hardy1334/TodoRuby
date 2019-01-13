a46266c9ffb65bf5829ae740a9ce6e97dd786b8f
hardy1334 <hardychugh34@gmail.com>
2019-01-13 23:17:34 +0530

    edit action added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -16,6 +16,20 @@ class TodosController < ApplicationController
 
     def show
         @todo = Todo.find(params[:id])
+    end   
+    
+    def edit
+        @todo = Todo.find(params[:id])       
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        if @todo.update(todo_params)
+            flash[:notice] = "Todo was sucessfully updated"
+            redirect_to todo_path(@todo)
+        else
+            render 'edit'
+        end      
     end    
 
     private
